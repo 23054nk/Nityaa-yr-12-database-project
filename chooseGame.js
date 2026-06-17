4c33ca0e57b633547fd31fa924171d64297fa36c
23054nk <23054nk@hvhs.school.nz>
2026-06-18 10:56:58 +1200

    save

---
+++ chooseGame.js
@@ -0,0 +1,8 @@
+
+/* IDK IF THIS WORKS (lINKS TO THE GAME PAGES)
+document.body.innerHTML += `
+    <a href="jurassic-chase/index.html">Play Jurassic Chase</a>
+    <br><br>
+    <a href="geo-dash/index.html">Play Geo Dash</a>
+`; */ 
+
--- script.js
+++ script.js
@@ -5,14 +5,40 @@ function writeForm(){
     // Get the form data
     const name = document.getElementById("name").value;
     const age = document.getElementById("age").value;
-    const email = document.getElementById("email").value;
+
     firebase.database().ref('users/' + name).set({
       name: name,
       age: age,
-      email: email
     })
 
 
 }
+var GLOBAL_user; // Google's user object
+
+// set up a listener for the login state of the user.
+function fb_login() {
+  firebase.auth().onAuthStateChanged(LOGIN_CALLBACK);
+}
 
 
+// run when the login state of the user changes
+function fb_handleLogin(_user) {
+  if (_user) {
+    console.log("User is logged in")
+    GLOBAL_user = _user; //save the user details object to a global variable
+  } else {
+    console.log("User is NOT logged in - Starting the popup process")
+    fb_popupLogin();
+  }
+}
+
+
+// run the google login popup
+function fb_popupLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user; // save the user details object to a global variable
+    console.log("User has logged in")
+  });
+}
